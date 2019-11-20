ddb4893e652858e5d740c65767ec7aafcbfcefeb
mindman79@hotmail.com <LwKmRWUc25hH>
2019-11-20 10:21:03 -0700

    Can add appointments

--- src/database/AppointmentDao.java
+++ src/database/AppointmentDao.java
@@ -7,12 +7,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import utils.DateTime;
 
+import javax.xml.bind.SchemaOutputResolver;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.util.TimeZone;
 
 import static controller.LoginScreenController.currentUser;
 import static database.DBConnection.conn;
@@ -42,7 +45,6 @@ public class AppointmentDao {
             while (result.next()) {
 
 
-
                 String appointmentTitle = result.getString("title");
                 String appointmentDescription = result.getString("description");
                 String appointmentLocation = result.getString("location");
@@ -50,6 +52,43 @@ public class AppointmentDao {
                 LocalDateTime appointmentEndTime = result.getTimestamp("end").toLocalDateTime();
                 Appointment appointment = new Appointment(appointmentTitle, appointmentDescription, appointmentLocation, appointmentStartTime, appointmentEndTime);
 
+
+                Appointments.add(appointment);
+
+            }
+
+            return Appointments;
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
+            return null;
+        }
+    }
+
+
+    public static ObservableList<Appointment> getWeeklyAppointments() {
+
+
+        Appointments.clear();
+
+
+        try {
+
+
+            String sqlStatement = "select * from appointment where start between utc_date + 0  and utc_date + 7" +
+                    "order by start desc;";
+
+            QueryManager.makeQuery(sqlStatement);
+            ResultSet result = QueryManager.getResult();
+
+            while (result.next()) {
+
+
+                String appointmentTitle = result.getString("title");
+                String appointmentDescription = result.getString("description");
+                String appointmentLocation = result.getString("location");
+                LocalDateTime appointmentStartTime = result.getTimestamp("start").toLocalDateTime();
+                LocalDateTime appointmentEndTime = result.getTimestamp("end").toLocalDateTime();
+                Appointment appointment = new Appointment(appointmentTitle, appointmentDescription, appointmentLocation, appointmentStartTime, appointmentEndTime);
 
 
 
@@ -65,10 +104,71 @@ public class AppointmentDao {
         }
     }
 
+    public static Appointment dateTimeConverter(Appointment appointment) {
 
+        //Get local Time Zone ID
+        ZoneId localZoneId = ZoneId.of(TimeZone.getDefault().getID());
+
+        //Get startDateTime
+        LocalDateTime appointmentStartDateTime = appointment.getStart();
+        //System.out.println("Appointment start date/time non-UTC: " + appointmentStartDateTime);
+        ZonedDateTime startLDT = ZonedDateTime.of(appointmentStartDateTime, localZoneId);
+
+
+        //Get endDateTime
+        LocalDateTime appointmentEndDateTime = appointment.getEnd();
+        ZonedDateTime endLDT = ZonedDateTime.of(appointmentEndDateTime, localZoneId);
+
+        Instant startTimeToUTC = startLDT.toInstant();
+        Instant endTimeToUTC = endLDT.toInstant();
+        Timestamp timestampStart = Timestamp.from(startTimeToUTC);
+        Timestamp timestampEnd = Timestamp.from(startTimeToUTC);
+
+
+        return appointment;
+    }
 
 
     public static void addAppointment(Appointment appointment) throws SQLException {
+
+        //Get local Time Zone ID
+        ZoneId localZoneId = ZoneId.of(TimeZone.getDefault().getID());
+
+        //Get startDateTime
+        LocalDateTime appointmentStartDateTime = appointment.getStart();
+        System.out.println("Appointment start date/time non-UTC: " + appointmentStartDateTime);
+        ZonedDateTime startLDT = ZonedDateTime.of(appointmentStartDateTime, localZoneId);
+
+
+        //Get endDateTime
+        LocalDateTime appointmentEndDateTime = appointment.getEnd();
+        ZonedDateTime endLDT = ZonedDateTime.of(appointmentEndDateTime, localZoneId);
+
+
+        //LocalTime localTime = LocalTime.now();
+        //LocalDateTime localDate = LocalDateTime.now();
+
+
+
+
+        Instant startTimeToUTC = startLDT.toInstant();
+        Instant endTimeToUTC = endLDT.toInstant();
+        Timestamp timestampStart = Timestamp.from(startTimeToUTC);
+        Timestamp timestampEnd = Timestamp.from(endTimeToUTC);
+
+//        System.out.println("Start time UTC: " + startTimeToUTC.truncatedTo(ChronoUnit.SECONDS));
+//        System.out.println("End time UTC " + endTimeToUTC.truncatedTo(ChronoUnit.MINUTES));
+
+//        String datetime = String.valueOf(startTimeToUTC);
+//        datetime = datetime.substring(0, datetime.length() - 4);
+//        System.out.println("date time test: " + datetime);
+
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm AA"); // your template here
+//        java.util.Date dateStr = formatter.parse(startTimeToUTC);
+
+//
+//        String fullPattern = "MM/dd/yyyy HH:mm AA";
+//        SimpleDateFormat fullDateFormat = new SimpleDateFormat(startTimeToUTC);
 
 
         int appointmentId = getNextApptId();
@@ -92,23 +192,19 @@ public class AppointmentDao {
             statement.setObject(7, appointment.getContact());
             statement.setObject(8, appointment.getType());
             statement.setObject(9, appointment.getUrl());
-            statement.setObject(10, appointment.getStart());
-            statement.setObject(11, appointment.getEnd());
+            statement.setObject(10, timestampStart);
+            statement.setObject(11, timestampEnd);
             statement.setObject(12, LocalDate.now());
             statement.setString(13, currentUser.getUserName());
             statement.setString(14, currentUser.getUserName());
             statement.executeUpdate();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("SQL Exception (address): " + e.getMessage());
         }
 
 
-
-
     }
-
 
 
     public static int getNextApptId() throws SQLException {
--- src/utils/DateTime.java
+++ src/utils/DateTime.java
@@ -1,6 +1,8 @@
 package utils;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 public class DateTime {
 
@@ -9,5 +11,14 @@ public class DateTime {
     private static String fullPattern = "MM/dd/yyyy HH:mm AA";
     public static SimpleDateFormat fullDateFormat = new SimpleDateFormat(fullPattern);
 
+
+    /*public static LocalDateTime convertUTCToLocal(LocalDateTime ldt) {
+
+     LocalDateTime input(ZoneOffset.UTC) = ldt;
+        ZoneOffset zoneOffset = zoneOffset.getRules().getOffset(ldt);
+
+
+
+}*/
 
 }
