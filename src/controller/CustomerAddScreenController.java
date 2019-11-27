f8daf74d8680d8589c73622e472f48040701385d
mindman79@hotmail.com <LwKmRWUc25hH>
2019-11-27 15:01:13 -0700

    Input validator works

--- src/controller/CustomerAddScreenController.java
+++ src/controller/CustomerAddScreenController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -22,6 +23,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class CustomerAddScreenController {
@@ -83,55 +85,90 @@ public class CustomerAddScreenController {
     void save_button_handler(ActionEvent event) throws IOException, SQLException {
 
 
-        setCountryIDFromName();
+        try {
+            setCountryIDFromName();
 
-        //int customerId = newCustomer.getCustomerId();
+            //int customerId = newCustomer.getCustomerId();
 
-        int cityId = CityDao.getNextCityId();
-        int addressId = AddressDao.getNextAddressId();
-        int customerId = CustomerDao.getNextCustomerID();
+            int cityId = CityDao.getNextCityId();
+            int addressId = AddressDao.getNextAddressId();
+            int customerId = CustomerDao.getNextCustomerID();
 
-        //Customer
-        newCustomer.setCustomerName(customer_name_field.getText());
-        newCustomer.setCustomerId(customerId);
-        newCustomer.setAddressId(addressId);
-        //newCustomer.setCustomerId(customerId);
-
-
-        //Address
-        custAddress.setAddressId(addressId);
-        custAddress.setAddress1(address_line_1_field.getText());
-        custAddress.setAddress2(address_line_2_field.getText());
-        custAddress.setPostalCode(postal_code_field.getText());
-        custAddress.setPhone(phone_number_field.getText());
-        custAddress.setCityId(cityId);
-
-        //City
-        custCity.setCity(city_field.getText());
-        custCity.setCityId(cityId);
-        custCity.setCountryId(countryId);
+            //Customer
+            newCustomer.setCustomerName(customer_name_field.getText());
+            newCustomer.setCustomerId(customerId);
+            newCustomer.setAddressId(addressId);
+            //newCustomer.setCustomerId(customerId);
 
 
-        CityDao.addCity(custCity);
-        AddressDao.addAddress(custAddress);
-        CustomerDao.addCustomer(newCustomer);
+            //Address
+            custAddress.setAddressId(addressId);
+            custAddress.setAddress1(address_line_1_field.getText());
+            custAddress.setAddress2(address_line_2_field.getText());
+            custAddress.setPostalCode(postal_code_field.getText());
+            custAddress.setPhone(phone_number_field.getText());
+            custAddress.setCityId(cityId);
+
+            //City
+            custCity.setCity(city_field.getText());
+            custCity.setCityId(cityId);
+            custCity.setCountryId(countryId);
+
+            //Customer input validation. Country selection validates with the NullPointerException in the try/catch block
+            if(customer_name_field.getText().trim().isEmpty()) {
+
+                alertGenerator("Customer name cannot be blank!");
+
+            } else if(address_line_1_field.getText().trim().isEmpty()){
+
+                alertGenerator("Address (first line) cannot be blank!");
+
+            } else if(city_field.getText().trim().isEmpty()){
+
+                alertGenerator("City name cannot be blank!");
+
+            } else if(postal_code_field.getText().trim().isEmpty()){
+
+                alertGenerator("Postal code cannot be blank!");
+
+            } else if(phone_number_field.getText().trim().isEmpty()){
+
+                alertGenerator("Phone number cannot be blank!");
+
+            } else {
 
 
-        //CountryDao.addCountry(custCountry);
+                CityDao.addCity(custCity);
+                AddressDao.addAddress(custAddress);
+                CustomerDao.addCustomer(newCustomer);
 
 
-        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(getClass().getResource("/view/CustomerScreen.fxml"));
-        stage.setScene(new Scene(scene));
-        stage.show();
+                stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+                scene = FXMLLoader.load(getClass().getResource("/view/CustomerScreen.fxml"));
+                stage.setScene(new Scene(scene));
+                stage.show();
 
-        System.out.println("Save button customerID test: " + newCustomer.getCustomerId());
+            }
+//            System.out.println("Save button customerID test: " + newCustomer.getCustomerId());
+//
+//            System.out.println("Save button addressID test: " + custAddress.getAddressId());
+//
+//            System.out.println("Save button cityID test: " + custCity.getCityId());
+//
+//            System.out.println("Save button countryID test: " + custCountry.getCountryId());
+        } catch (SQLException e) {
+            e.printStackTrace();
 
-        System.out.println("Save button addressID test: " + custAddress.getAddressId());
+        } catch (IOException e) {
+            e.printStackTrace();
 
-        System.out.println("Save button cityID test: " + custCity.getCityId());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
 
-        System.out.println("Save button countryID test: " + custCountry.getCountryId());
+            alertGenerator("Country must be selected!");
+
+
+        }
 
 
     }
@@ -197,5 +234,14 @@ public class CustomerAddScreenController {
 
     }
 
+    public void alertGenerator(String message) {
+
+        Alert apptAlert = new Alert(Alert.AlertType.INFORMATION);
+        apptAlert.setTitle(message);
+        apptAlert.setHeaderText(message);
+        apptAlert.showAndWait();
+
+
+    }
 
 }
